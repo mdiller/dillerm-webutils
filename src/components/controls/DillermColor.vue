70d7a0f4c17f442a1cba8a15902885e8f76e6eac
Malcolm Diller <malcolm@dillerm.io>
2022-10-06 23:45:37 -0700

    updated color control to be better

--- src/components/controls/DillermColor.vue
+++ src/components/controls/DillermColor.vue
@@ -1,20 +1,36 @@
 <template>
 	<div class="dillerm dillerm-control dillerm-color" :style="{ background: value }">
+		<div v-if="!value">
+			{{ placeholder }}
+		</div>
 		<input type="color" v-model="val" />
-		<span>
+		<span v-if="value">
 			{{value}}
 		</span>
+		<CrossIcon  
+			class="dillerm-input-clear"
+			v-if="value" 
+			@click="value = null" />
 	</div>
 </template>
 
 <script>
+import CrossIcon from "../../assets/cross.svg?component";
+
 export default {
 	name: 'dillerm-color',
 	props: {
 		value: {
 			type: String,
 			required: true
+		},
+		placeholder: {
+			type: String,
+			default: "Select a color..."
 		}
+	},
+	components: {
+		CrossIcon
 	},
 	data() {
 		return {
@@ -33,9 +49,31 @@ export default {
 </script>
 
 <style lang="scss">
-@import "../../base.scss";
 
+
+$button-icon-side-padding: 5px;
+$button-icon-size: 20px;
 .dillerm-color {
+	position: relative;
+	width: 100%;
+	height: var(--input-height);
+	border: var(--input-border);
+	border-radius: var(--input-border-radius);
+	position: relative;
+	background: var(--input-background);
+
+	> div {
+		position: absolute;
+		left: 0;
+		right: 0;
+		top: 0;
+		bottom: 0;
+		line-height: var(--input-height);
+		pointer-events: none;
+		padding-left: var(--input-padding-rl);
+		color: grey;
+	}
+
 	input {
 		opacity: 0;
 		position: absolute;
@@ -48,16 +86,6 @@ export default {
 		cursor: pointer;
 	}
 
-	& {
-		position: relative;
-		width: 100%;
-		height: $input-height;
-		border: $input-border;
-		border-radius: $input-border-radius;
-		position: relative;
-		background: $input-background;
-	}
-
 	$spacing: 4px;
 
 	span {
@@ -65,10 +93,10 @@ export default {
 		right: $spacing;
 		top: $spacing;
 		bottom: $spacing;
-		line-height: calc(#{$input-line-height} - (#{$spacing} * 2));
-		padding: $input-padding;
-		background: $input-background;
-		border-radius: $input-border-radius;
+		line-height: calc(#{var(--input-line-height)} - (#{$spacing} * 2));
+		padding: var(--input-padding);
+		background: var(--input-background);
+		border-radius: var(--input-border-radius);
 		opacity: 0.75;
 		pointer-events: none;
 		color: var(--input-color);
@@ -81,6 +109,26 @@ export default {
 	&:hover > span,
 	&:focus-within > span {
 		opacity: 0.95;
+	}
+	
+	svg.dillerm-input-clear {
+		width: $button-icon-size;
+		height: $button-icon-size;
+		position: absolute;
+		top: calc((#{var(--input-height)} - #{$button-icon-size}) / 2);
+		right: 0;
+		cursor: pointer;
+		fill: var(--input-color);
+		opacity: 0.75;
+		transition: 0.25s;
+		filter: drop-shadow(0px 0px 3px black);
+
+		right: 80px;
+
+		&:hover {
+			fill: red;
+			opacity: 1;
+		}
 	}
 }
 
